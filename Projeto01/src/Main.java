c95bd8f05e795e7235ac130fb4e91ba053a9a854
Lucas Guerhardt PInheiro <94179839+pinheiroo@users.noreply.github.com>
2023-02-13 19:31:59 -0300

    Update Main.java

--- Projeto01/src/Main.java
+++ Projeto01/src/Main.java
@@ -1,7 +1,12 @@
+import java.io.*;
 class Main {
-
-    public static void main(String[] args){
-        
+    public static void main(String[] args) throws IOException{
+        try {
+            GrafoAtores grafo = new GrafoAtores("teste.txt");
+            grafo.printGrafoNomes();
+            grafo.printFilmeAtor();
+        } catch (IOException e) {
+            System.out.println("Ocorreu um erro na leitura do arquivo movies.txt");
+        }
     }
-
 }
