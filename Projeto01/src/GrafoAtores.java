f1ecac45dd1154e93445ef12c113b2410a959c28
Lucas Guerhardt PInheiro <94179839+pinheiroo@users.noreply.github.com>
2023-02-21 15:39:48 -0300

    Update GrafoAtores.java

--- Projeto01/src/GrafoAtores.java
+++ Projeto01/src/GrafoAtores.java
@@ -1,5 +1,5 @@
 import java.io.*;
-import java.util.ArrayList;
+import java.util.*;
 
 public class GrafoAtores {
 
@@ -124,12 +124,89 @@ public class GrafoAtores {
         }
 
     }
+
     public void printFilmeAtor(){
-        System.out.printf("\nFilmes estrados por:\n");
+        System.out.printf("\nFilmes estrelados por:\n");
         for(int i=0;i<FilmeAtor.size();i++){
             System.out.println(atores.get(i) + "->" + FilmeAtor.get(i));
         }
 
     }
 
+    public void printFrequencia(String ator){
+        int indice = indice(ator);
+        countFreq(G().BFS(indice), G().BFS(indice).length);
+    }
+
+    public void countFreq(int arr[], int n){
+
+        boolean visited[] = new boolean[n];
+
+        Arrays.fill(visited, false);
+
+        System.out.println("Numero Bacon     Frequencias\n------------------------------");
+        
+        int infinito = 0;
+
+        for (int i = 0; i < n; i++) {
+    
+            if (visited[i] == true){
+                continue;
+            }
+    
+            int count = 1;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] == arr[j]) {
+                    visited[j] = true;
+                    count++;
+                }
+            }
+            if(arr[i]==0){
+               infinito = count-1;
+               System.out.println("     0                1");
+            }else{
+                System.out.println("     "+arr[i] + "                " + count);
+            }
+            
+        }
+        System.out.println("  Infinito            " + infinito);
+    }
+ 
+
+
+
+    public void printCaminhoAtores(String ator, String origem){
+        int[] vetor = grafo.BFS(indice(origem));
+        System.out.println(ator+" tem número de "+origem+" = "+vetor[indice(ator)]);
+
+        caminhoAtores(ator,origem);
+    }
+
+    public int caminhoAtores(String ator, String origem){
+        int ator1 = indice(ator);
+        int ator2 = indice(origem);
+        int[] vetor = grafo.BFS(ator2);
+        int prox=0;
+
+        if(ator1==ator2){
+            return 0;
+        }
+        else{
+            for(int i=0; i<grafo.adj(ator1).size(); i++){
+                if(vetor[grafo.adj(ator1).get(i)]==vetor[ator1]-1){
+                    prox = grafo.adj(ator1).get(i);
+                    for(int j=0; j<FilmeAtor.get(prox).size();j++){
+                        for(int k=0; k<FilmeAtor.get(ator1).size(); k++){
+                            if(FilmeAtor.get(prox).get(j).equals(FilmeAtor.get(ator1).get(k))){
+                                System.out.println(ator+" fez "+FilmeAtor.get(ator1).get(k)+" com "+nome(prox));
+                            }
+
+                        }
+
+                    }
+                }
+            }
+            return caminhoAtores(nome(prox),origem);
+        }
+    }
 }
