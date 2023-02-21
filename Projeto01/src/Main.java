c581b9ae7d68b5097786b78bc6ee236e4f782c00
Lucas Guerhardt PInheiro <94179839+pinheiroo@users.noreply.github.com>
2023-02-21 15:38:34 -0300

    Update Main.java

--- Projeto01/src/Main.java
+++ Projeto01/src/Main.java
@@ -1,12 +1,40 @@
-import java.io.*;
-class Main {
-    public static void main(String[] args) throws IOException{
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
         try {
-            GrafoAtores grafo = new GrafoAtores("teste.txt");
-            grafo.printGrafoNomes();
-            grafo.printFilmeAtor();
+            Scanner in = new Scanner(System.in);
+
+            
+            String nomeArquivo;
+            System.out.println("Digite o nome do arquivo: ");
+            nomeArquivo = in.nextLine();
+            String nomeBacon;
+            System.out.println("Digite o nome do ator a ser buscado (Exemplo Bacon, Kevin): ");
+            nomeBacon = in.nextLine();
+            String atorLigar;
+            System.out.println("Digite o nome do ator para ligar à bacon(Exemplo Dane, Cynthia): ");
+            atorLigar = in.nextLine();
+            in.close();
+
+
+            GrafoAtores grafoAtores = new GrafoAtores(nomeArquivo);
+            System.out.println("Caminho do ator:");
+            System.out.println();
+            System.out.println();
+            grafoAtores.printCaminhoAtores(atorLigar,nomeBacon);
+            System.out.println();
+            System.out.println();
+            System.out.println("Frequencia de Bacon:");
+            System.out.println();
+            System.out.println();
+            grafoAtores.printFrequencia(nomeBacon);
+            System.out.println();
+            System.out.println();
+
         } catch (IOException e) {
-            System.out.println("Ocorreu um erro na leitura do arquivo movies.txt");
+            e.printStackTrace();
         }
     }
 }
