d0c19ea7752429734f336cff45c1018d72130ec2
Baden <56354660+BadenP@users.noreply.github.com>
2023-04-05 15:46:50 -0300

    Update Grafo.java

--- Projeto02/Grafo.java
+++ Projeto02/Grafo.java
@@ -105,7 +105,7 @@ class Grafo{
         return dist;
     }
 
-    public int max(int[] d) {
+    private int max(int[] d) {
         int max = d[0]; 
         for (int i = 0; i < d.length; i++) {
             if (d[i] > max) {
